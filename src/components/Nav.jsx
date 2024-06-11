aaafedaacc9823c5be7b4b2dc0f933663aa8213b
Jakub <jakub.tomczyk.2005@gmail.com>
2024-06-11 11:36:13 +0200

    working shadcn + tailwind

---
+++ src/components/Nav.jsx
@@ -0,0 +1,59 @@
+import Image from "next/image";
+import Link from "next/link";
+// import { getServerSession } from "next-auth";
+// import { options } from "@/app/api/auth/[...nextauth]/options";
+async function Nav() {
+  // const session = await getServerSession(options);
+  const session = false;
+  return (
+    <div className="sticky top-0 z-10">
+      <div className="flex flex-row justify-center py-1 bg-third">
+        <p className="text-sm font-medium text-white">
+          Welcome to TheBrains.com - a library of knowledge from YouTube
+        </p>
+      </div>
+      <div className="flex gap-4 justify-between items-center px-20 border-b-2 border-b-gray-400 font-semibold bg-beige">
+        <div className="flex">
+          <div className="flex items-center gap-1 py-3 px-4 hover:border-b-black hover:border-b-2">
+            <Image src="/brain.svg" width={25} height={25} />
+            <Link href="/" className="font-bold">
+              TheBrain.com
+            </Link>
+          </div>
+          <div className="flex">
+            <Link
+              href="/search"
+              className="py-3 px-4 hover:border-b-black hover:border-b-2"
+            >
+              Better Search
+            </Link>
+          </div>
+        </div>
+        <div className="flex gap-4 items-center">
+          {session ? (
+            <Link
+              href="/auth/signout"
+              className="px-4 py-2 border-[1px] border-black rounded-md transition-all hover:bg-black hover:text-beige hover:scale-[1.03]"
+            >
+              Sign out
+            </Link>
+          ) : (
+            <>
+              <Link href="/auth/login" className="px-4 py-2">
+                Sign in
+              </Link>
+              <Link
+                href="/auth/register"
+                className="px-4 py-2 border-[1px] border-black rounded-md transition-all hover:bg-black hover:text-beige hover:scale-[1.03]"
+              >
+                Sign up
+              </Link>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
