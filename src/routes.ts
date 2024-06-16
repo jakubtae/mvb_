17b5cc027782f6412ba02e1060186a705586b85e
Jakub <jakub.tomczyk.2005@gmail.com>
2024-06-16 20:26:14 +0200

    working template

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@
  * Those routes do not require authentication
  * @type {string[]}
  */
-export const publicRoutes = ["/", "/search"];
+export const publicRoutes = ["/", "/tab1", "/tab2"];
 
 /**
  * An array of routes that are used for authentication
